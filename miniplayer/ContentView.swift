cfd035e5f94564f43ba7c00e2f8425a86499a71f
yoshiwa1337 <forbes.y2403@gmail.com>
2025-08-09 14:02:52 +0100

    base transition between views

--- miniplayer/ContentView.swift
+++ miniplayer/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPlayerExpanded = false
+
+    func player(){
+        withAnimation(.spring()){
+            isPlayerExpanded.toggle()
+        }
+    }
+
+    var body: some View {
+        ZStack{
+            Color.green
+            
+            VStack{
+                Button("show player"){
+                    withAnimation(.spring()){
+                        isPlayerExpanded = true
+                    }
+                }
+            }
+            
+            if isPlayerExpanded {
+                FullscreenPlayer(onArrowTap: player).transition(.move(edge: .top).combined(with: .opacity)).zIndex(1)
+            }
+        }
+    }
+    
+}
+struct FullscreenPlayer: View {
+    let onArrowTap: () -> Void
+    
     var body: some View {
         ZStack(alignment: .top){
             LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
             HStack{
-                Image(systemName: "chevron.up").foregroundColor(.white)
-                    .padding(.horizontal)
+                Button(action: onArrowTap){
+                    Image(systemName: "chevron.up").foregroundColor(.white)
+                        .padding(.horizontal)
+                }
                 Spacer()
                 Text("Now playing").foregroundColor(.white)
                 Spacer()
