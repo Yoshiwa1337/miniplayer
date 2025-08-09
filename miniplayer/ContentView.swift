4b74590ce1bd08d03fc8843b08679b3643227035
yoshiwa1337 <forbes.y2403@gmail.com>
2025-08-09 10:45:28 +0100

    Initial Commit

---
+++ miniplayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  miniplayer
+//
+//  Created by Yoshi Forbes on 09/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ miniplayer/miniplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniplayerApp.swift
+//  miniplayer
+//
+//  Created by Yoshi Forbes on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct miniplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
