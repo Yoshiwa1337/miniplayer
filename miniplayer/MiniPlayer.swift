15c6b1a4c4ef61c735c9b6f3b42bd82c998f6cdd
yoshiwa1337 <forbes.y2403@gmail.com>
2025-08-10 02:45:45 +0100

    miniplayer start

---
+++ miniplayer/MiniPlayer.swift
@@ -0,0 +1,42 @@
+//
+//  MiniPlayer.swift
+//  miniplayer
+//
+//  Created by Yoshi Forbes on 10/08/2025.
+//
+
+import SwiftUI
+
+struct MiniPlayer: View {
+    @State private var progress = 50.0
+    var body: some View {
+        VStack{
+            
+            HStack(spacing: 15){
+                //img
+                Image("universe")
+                    .resizable()
+                    .cornerRadius(15)
+                    .frame(width:55, height: 55)
+                    .padding(.leading, 30)
+                //details
+                Spacer(minLength: 0)
+                VStack{
+                    Text("Song name")
+                    Text("Stick Man")
+                }.frame(alignment: .center)
+                //start button
+                Spacer(minLength: 0)
+                Image(systemName: "play.circle.fill")
+                    .padding(.trailing, 30)
+            }
+            Slider(value: $progress, in: 0...100)
+                .padding(.horizontal)
+                .padding(.vertical, -19.0)
+        }
+    }
+}
+
+#Preview {
+    MiniPlayer()
+}
